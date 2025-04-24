64e5be1fbc3b029dca897ef32e41d13d174e02b5
Emils <emils@gmail.com>
2025-04-24 14:43:16 +0300

    testing Laravel routes and views

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/test', function () {
+//     // return view('welcome');
+// });
+
+// Route::get('/test', function () {
+//     echo "<h1>test</h1>";
+//     $name = "Mareks";
+//     echo $name;
+// });
+
+Route::get('/test', function () {
+    return view('test', ['variable' => "my variable text value"]);
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/post', function () {
+    return view('post');
+});
